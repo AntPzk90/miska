592a2c792c8b71254e3eb8abdb0e1d7dee18b544
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-07-29 11:19:21 +0300

    add animate

--- src/js/main.js
+++ src/js/main.js
@@ -22,8 +22,14 @@ window.addEventListener("scroll", function(evt){
     //bg.style.opacity = 1 - (0 + "." + scrolling); 
     this.console.log(scrolling);
 });
-ScrollReveal().reveal('.squares', { mobile: false });
+// header animate
+ScrollReveal().reveal(".header__logo",{delay: 200,duration: 200,distance:"50px",reset: true});
+ScrollReveal().reveal(".header__main-nav",{delay: 200,duration: 200,distance:"50px",reset: true});
+ScrollReveal().reveal(".header__user-wrapper",{delay: 200,duration: 200,distance:"50px",reset: true});
+ScrollReveal().reveal(".header__delivery",{delay: 200,duration: 200,distance:"50px",reset: true});
+ScrollReveal().reveal(".header__more-list",{delay: 200,duration: 200,distance:"50px",reset: true});
 ScrollReveal().reveal(".represent__list",{delay: 375,duration: 400,distance:"50px",reset: true});
+//index animate
 //ScrollReveal().reveal(".week-product__title-wrapper",{delay: 550,duration: 550,reset: true});
 ScrollReveal().reveal(".week-product__dscr",{delay: 300,duration: 300,distance:"50px",reset: true});
 ScrollReveal().reveal(".week-prouct__data",{delay: 300,duration: 300,distance:"50px",reset: true});
@@ -37,6 +43,24 @@ ScrollReveal().reveal(".reviews__btn",{delay: 300,duration: 300,distance:"50px",
 ScrollReveal().reveal(".contacts__data-table",{delay: 300,duration: 300,distance:"50px",reset: true});
 ScrollReveal().reveal(".contacts__btn",{delay: 300,duration: 300,distance:"50px",reset: true});
 ScrollReveal().reveal(".contacts__map",{delay: 300,duration: 300,distance:"50px",reset: true});
+//catalog animate
+ScrollReveal().reveal(".catalogs-items__list-item:nth-child(1)",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".catalogs-items__list-item:nth-child(2)",{delay: 350,duration: 350,distance:"50px",reset: true});
+ScrollReveal().reveal(".catalogs-items__list-item:nth-child(3)",{delay: 400,duration: 400,distance:"50px",reset: true});
+ScrollReveal().reveal(".catalog-video__video-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".catalog-video__txt",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".catalog-video__btn",{delay: 300,duration: 300,distance:"50px",reset: true});
+//form animate
+ScrollReveal().reveal(".form__radio-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__checkbox-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__text-name-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__fieldset-tel-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__fieldset-email-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__fieldset-dop-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__btn-wrapper",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".legend",{delay: 300,duration: 300,distance:"50px",reset: true});
+ScrollReveal().reveal(".form__slogan",{delay: 300,duration: 300,distance:"50px",reset: true});
+//footer animate
 ScrollReveal().reveal(".footer__logo",{delay: 300,duration: 300,distance:"50px",reset: true});
 ScrollReveal().reveal(".fooetr__social-list",{delay: 300,duration: 300,distance:"50px",reset: true});
 ScrollReveal().reveal(".footer__developed-by",{delay: 300,duration: 300,distance:"50px",reset: true});
